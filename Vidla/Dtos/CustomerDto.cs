0a21e70683a852af664a7a801e6e6f1d9dc789a7
samart <samart@TUCC-CEC260>
2019-10-17 11:52:35 -0500

    Created Customer DTO model

---
+++ Vidla/Dtos/CustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidla.Models;
+
+namespace Vidla.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        
+        public byte MembershipTypeId { get; set; }
+
+        
+
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
