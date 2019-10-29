25b7c2847ddec0b45e91f321f2fb1d2c2e7d757f
Santiago Martínez L <santimart2006@hotmail.com>
2019-10-29 00:09:35 -0500

    Build a feauture end to end beginning

--- Vidla/Controllers/AccountController.cs
+++ Vidla/Controllers/AccountController.cs
@@ -386,7 +386,8 @@ namespace Vidla.Controllers
                 var user = new ApplicationUser { 
                     UserName = model.Email, 
                     Email = model.Email,
-                    DrivingLicense = model.DrivingLicense
+                    DrivingLicense = model.DrivingLicense,
+                    Phone = model.Phone
                 };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
